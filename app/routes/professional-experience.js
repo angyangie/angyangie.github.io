488ab7f916e06eac2bac2f4d604d5fb64fda56a6
angyangie <angie.ramirez12@gmail.com>
2016-08-06 11:41:21 -0400

    Professional experience section finished

--- app/routes/professional-experience.js
+++ app/routes/professional-experience.js
@@ -8,21 +8,21 @@ export default Ember.Route.extend({
       url: "http://www.nimblestack.io",
       subtitle: "Full Stack Software Developer",
       dates: "June 2016 - Present",
-      description: "Nimblestack specializes in emergent technology, simplifying and automating software. We develop software that makes other software, taking care of both frontend and backend development using Javascript, React.js, Node.js, and MongoDB."
+      description: "NimbleStack specializes in emergent technology, simplifying and automating software. At NimbleStack, I develop front-end and back-end of chatbots using Javascript, React.js, Redux, NodeJS, and MongoDB. I integrate Facebook, Gmail, IBM Watson, and other machine learning/artificial intelligence APIs into chatbots."
     },
     {
       title: "SeaChange Capital Partners",
       url: "http://seachangecap.org",
       subtitle: "Associate & Fellow",
       dates: "May 2012 - June 2014",
-      description: "SeaChange Capital Partners enables transactions that increase the impact of nonprofits while offering leveraged funding opportunities for donors. SeaChange manages a series of funds that make grants, loans, and investments."
+      description: "SeaChange Capital Partners enables transactions that increase the impact of nonprofits while offering leveraged funding opportunities for donors. SeaChange manages a series of funds that make grants, loans, and investments. At SeaChange, I set-up, managed, and maintained salesforce data management system and SeaChange website. I assisted in financial modeling, restructuring, and strategy assessment for nonprofits considering significant transactions, including restructurings, growth capital campaigns, and site replications. I also sourced and recommend early stage, high-potential, underfunded programs and new technology initiatives for future investment by the Heckscher Foundation for Children ($300 million in net assets)."
     },
     {
       title: "Teach For America",
       url: "https://www.teachforamerica.org/",
       subtitle: "Bilingual Ancient History Teacher",
       dates: "May 2012 - June 2014",
-      description: "Teach For America enlists high-achieving recent college graduates and professionals to teach in low-income communities throughout the United States."
+      description: "Teach For America (TFA) enlists high-achieving recent college graduates and professionals to teach in low-income communities throughout the United States. As a TFA Corps Member, I analyzed trends in student test and classroom performance data to inform and influence future pedagogical decisions, using education technology tools like Remind101, ClassDojo, JumpRope, Achieve3000, and PowerSchool. I self-developed class website, lesson plans, class materials, and curriculum for 125 students a year in Spanish and English."
     }
     ]
   }
